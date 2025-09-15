bc32383a60d438bb8fbc23f0609f161d2f7e4bff
John Cedrek Alindao-UCM <johnomiple@gmail.com>
2025-09-15 08:56:48 +0800

    Add files via upload

---
+++ CHAPTER 3/CompanyInfo.java
@@ -0,0 +1,13 @@
+class  CompanyInfo{
+
+   public static void main(String[] args){
+      displayHours();
+   }
+   
+   public static void displayHours(){
+      System.out.println("Smart  Electronics");
+      System.out.println("Monday - Friday 8 am to 6pm");
+      System.out.println("saturday   8 am to noon ");
+      System.out.println("sunday     closed");
+   }
+}
