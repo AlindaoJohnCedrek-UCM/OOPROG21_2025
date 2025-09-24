3b7fd85bd5adb09f376e7e0e02a337f237bf4d78
John Cedrek Alindao-UCM <johnomiple@gmail.com>
2025-09-24 08:04:28 +0800

    Add files via upload

---
+++ CHAPTER_4/DebugPen.java
@@ -0,0 +1,34 @@
+public class DebugPen {
+    private String color;
+    private String point;
+
+    public DebugPen() {
+        color = "black";
+        point = "fine";
+    }
+
+    public DebugPen(String color, String point) {
+        this.color = color;
+        this.point = point;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getPoint() {
+        return point;
+    }
+    
+    public static void main(String[] a){
+         
+         DebugPen myDebugPen = new DebugPen();
+         
+         System.out.println("T-shirt" + "\n" +  
+                            "Color: " + myDebugPen.getColor() + "\n" + 
+                            "Point: " + myDebugPen.getPoint() + "\n"
+         );
+    
+    
+    }
+}
